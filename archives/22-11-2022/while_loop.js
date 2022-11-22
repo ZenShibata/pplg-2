3b8e334b5da2abdff98015ba7fbf68997451c78d
ZenShibata <zen@ayato.life>
2022-11-22 11:49:35 +0700

    feat: add while loop

--- archives/22-11-2022/while_loop.js
+++ archives/22-11-2022/while_loop.js
@@ -13,8 +13,11 @@ const readline = createInterface(process.stdin, process.stdout);
         throw new RangeError("Please input a valid number above 0");
     }
 
-    for (let i = 0; i <= range; i++) {
+    console.log(`Berikut adalah bilangan genap dari 0 - ${range}`);
+    let i = 0;
+    while (i <= range) {
         if (i % 2 === 0) console.log(i);
+        i++;
     }
 
     readline.close();
